892425a905d6e834d92d2d25f583930ad57f3668
vivianrebane <vivian.rebane@gmail.com>
2024-07-08 16:08:56 +0300

    Project Setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationTest : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
-        // testing
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
